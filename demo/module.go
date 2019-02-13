2d619b27374fc83adb7cb0dbc21760624e863bd1
王晓鹏 <wangxiaopeng@xiaomi.com>
2019-02-13 15:39:39 +0800

    add export package

---
+++ demo/module.go
@@ -0,0 +1,9 @@
+package demo
+
+import (
+	"fmt"
+)
+
+func Hi() {
+	fmt.Println("hello module test!")
+}
--- hello.go
+++ hello.go
@@ -3,8 +3,10 @@ package main
 import (
 	"fmt"
 	"rsc.io/quote"
+	"github.com/wxpjimmy/hello/demo"
 )
 
 func main() {
 	fmt.Println(quote.Hello())
+	demo.Hi()
 }
